616470a51881b635b28cbcb770606b1df5cd6ff0
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-06-06 17:54:44 +0300

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace _in_list(my_list, idx, element):
-        if idx < o or idx >= len(my_list):
+        if idx < 0 or idx >= len(my_list):
                 return my_list
         my_list[idx] = element
         return my_list
