23fc742e5e62903618449cb1efba62d5fd832547
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-06-07 04:03:10 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     nl = my_list[:]
-    if 0 > idx >= len(my_list):
-        return my_list
+    if 0 > idx >= len(nl):
+        return nl
     elif:
         nl[idx] = element
         return nl
