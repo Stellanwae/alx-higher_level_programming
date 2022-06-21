4d775640b56cd3904fad100a90771988d5aeec6d
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-06-21 05:13:44 +0300

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    for i in value:
         try:
             print("{:d}".format(value), end="")
-        except TypeError:
+        except (TypeError, ValueError):
             return False
+        else:
+            return True
