ea71a885e8696945dc61d0a2de3f38ca8b3e4f4d
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-06-28 05:48:57 +0300

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,7 +48,4 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return (2(self.__width + self.__height))
-
-    
-
+            return (self.__width * 2) + (self.__height * 2)
