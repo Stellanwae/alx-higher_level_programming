22aa4f766189863eec8ffdbf7bee3eb8896f5ee6
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-06-28 05:59:17 +0300

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,2 +1,58 @@
 #!/usr/bin/python3
+"""
+Defines the class Rectangle
+"""
 
+
+class Rectangle:
+    """Represents the rectangle"""
+    def __init__(self, width=0, height=0):
+        """initialise width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """To return self"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter that defines the private attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """to return self"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Public instance, want rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Public instance to return perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+    def __str__(self):
+        """returns printable ..."""" 
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#"*self.__width for j in range(self.__height))
+        return string
+        
