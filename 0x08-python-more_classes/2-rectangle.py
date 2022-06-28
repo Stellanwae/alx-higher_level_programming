409e213ce5ee7183a6505ee0556d04fd8501d267
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-06-28 05:38:34 +0300

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -37,6 +37,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0)
+        self.__height = value
 
     def area(self):
         """Public instance, want rectangle area"""
