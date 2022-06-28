6a85c4beffa28aeb6e43db55808bcc589653033b
Stellanwae <stellanic2011@gmail.com>
2022-06-28 05:32:04 +0300

    area

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!usr/bin/python3
+"""
+Defines the class Rectangle
+"""
+
+
+class Rectangle:
+    def __init__(self, width = 0, height = 0):
+        """initialise width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """To return self"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter that defines the private attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """to return self"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0)
+
+    def area(self):
+        """Public instance, want rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Public instance to return perimeter
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2(self.__width + self.__height))
+
+    
+
