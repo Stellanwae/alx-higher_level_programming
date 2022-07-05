3e720b818f62082839580b8b91024a14b1f96e61
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2022-07-05 05:53:43 +0300

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,6 @@ Module has the function is_same_class
 """
 
 
-def is_same_class(obj, a_class):
+def is_same_class(a, a_class):
     """true if object is in same class as a_class returns false when otherwise"""
-    return(type(obj) == a_class)
+    return(type(a) == a_class)
