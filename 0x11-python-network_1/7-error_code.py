0b1aeb0f29ad93ba3d63b917cb6fbae34cad2ed4
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-03-31 16:03:34 +0300

    Create 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes a url 
+sneds request by url 
+display body of response
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
