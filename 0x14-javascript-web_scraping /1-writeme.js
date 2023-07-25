52944654e3d1398f896100a0fc76c0cc2f70d6d3
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-07-25 18:19:36 +0300

    Create 1-writeme.js

---
+++ 0x14-javascript-web_scraping /1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(file, str, 'utf-8', function (error) {
+  if (error) console.log(error);
+});
